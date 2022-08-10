3504703cfbad8de0484880bbe7b51f63f217545b
FedeBenvenutto <benvenutto.federico@gmail.com>
2022-08-10 17:56:43 -0300

    modificada screen ver gastos y nuevo gasto

--- screens/Nuevogasto.js
+++ screens/Nuevogasto.js
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
 import { View, Text, TextInput, SafeAreaView, StyleSheet} from 'react-native';
 import { Button } from '@rneui/themed';
-import SelectList from 'react-native-dropdown-select-list';
 import { db } from '../database/firebase.js';
 import { addDoc, collection} from 'firebase/firestore';
+import SelectDropdown from 'react-native-select-dropdown'
 
 const Nuevogasto = (props) => {
-    const [selected1, setSelected1] = useState("");
-    const [selected2, setSelected2] = useState("");
     const [gasto, setGasto]= useState({
       Monto: "",
       Categoria: "",
       FormadePago: "",
       Comentario: "",
+      CategoriaIndex: "",
+      FormadePagoIndex: ""
+
     });
     const saveNewGasto = async () => {
       if (gasto.Monto === "") {
@@ -25,6 +26,8 @@ const Nuevogasto = (props) => {
             Categoria: gasto.Categoria,
             FormadePago: gasto.FormadePago,
             Comentario: gasto.Comentario,
+            CategoriaIndex: gasto.CategoriaIndex,
+            FormadePagoIndex: gasto.FormadePagoIndex,
             createdAt: new Date()
           });
           setGasto({
@@ -32,34 +35,59 @@ const Nuevogasto = (props) => {
             Categoria: "",
             FormadePago: "",
             Comentario: "",
+            CategoriaIndex: "",
+            FormadePagoIndex: ""
            })
           alert("Agregado");
         } catch (e) {
           console.log(e)
           alert(e)} } };
+
+      const categorias = [
+            'Salud',
+            'Supermercado',
+            'Niñera/Limpieza',
+            'Colegio',
+            'Verdulería',
+            'Impuestos',
+            'Pago Tarjeta de Crédito',
+            'Ropa',
+            'Carnicería',
+            'Librería',
+            'Otros gastos'
+        ];
         
-    const categorias = [
-        {key:'1',value:'Salud'},
-        {key:'2',value:'Supermercado'},
-        {key:'3',value:'Niñera/Limpieza'},
-        {key:'4',value:'Colegio'},
-        {key:'5',value:'Verdulería'},
-        {key:'6',value:'Impuestos'},
-        {key:'7',value:'Pago Tarjeta de Crédito'},
-        {key:'8',value:'Ropa'},
-        {key:'9',value:'Carnicería'},
-        {key:'10',value:'Librería'},
-        {key:'11',value:'Otros gastos'}
-    ];
+    // const categorias = [
+    //     {key:'1',value:'Salud'},
+    //     {key:'2',value:'Supermercado'},
+    //     {key:'3',value:'Niñera/Limpieza'},
+    //     {key:'4',value:'Colegio'},
+    //     {key:'5',value:'Verdulería'},
+    //     {key:'6',value:'Impuestos'},
+    //     {key:'7',value:'Pago Tarjeta de Crédito'},
+    //     {key:'8',value:'Ropa'},
+    //     {key:'9',value:'Carnicería'},
+    //     {key:'10',value:'Librería'},
+    //     {key:'11',value:'Otros gastos'}
+    // ];
+  //   const formadePago = [
+  //     {key:'1',value:'Tarjeta de Crédito'},
+  //     {key:'2',value:'Tarjeta de Débito'},
+  //     {key:'3',value:'Efectivo'},
+  //     {key:'4',value:'Billetera Santa Fe con descuesto'},
+  //     {key:'5',value:'Billetera Santa Fe sin descuesto'},
+  //     {key:'6',value:'Personal Pay con descuento '},
+  //     {key:'7',value:'Donaciones'},
+  //     {key:'8',value:'Otros'},
+  // ]
     const formadePago = [
-        {key:'1',value:'Tarjeta de Crédito'},
-        {key:'2',value:'Tarjeta de Débito'},
-        {key:'3',value:'Efectivo'},
-        {key:'4',value:'Billetera Santa Fe con descuesto'},
-        {key:'5',value:'Billetera Santa Fe sin descuesto'},
-        {key:'6',value:'Personal Pay con descuento '},
-        {key:'7',value:'Donaciones'},
-        {key:'8',value:'Otros'},
+        'Tarjeta de Crédito',
+        'Tarjeta de Débito',
+        'Efectivo',
+        'Billetera Santa Fe',
+        'Personal Pay con descuento ',
+        'Donaciones',
+        'Otros',
     ];
   return (
     
@@ -78,22 +106,27 @@ const Nuevogasto = (props) => {
       </SafeAreaView>
       <SafeAreaView style={styles.diasyhorarios}>
         <Text style={styles.textdias}> Categoría</Text>
-        <SelectList 
-        search={false}
-        setSelected={setSelected1} 
-        data={categorias} 
-        onSelect={() => setGasto({...gasto, "Categoria": selected1})} 
-        boxStyles={{marginStart: 65}}
+        <SelectDropdown 
+        data={categorias}
+        onSelect={(selectedItem, index) => {
+          setGasto({...gasto, "Categoria": selectedItem, "CategoriaIndex": index})
+        }}
+        buttonStyle={styles.dropdown}
+        defaultValueByIndex={gasto.CategoriaIndex}
+        // boxStyles={{marginStart: 65}}
+        // defaultValueByIndex= {""}
         />
       </SafeAreaView>
       <SafeAreaView style={styles.diasyhorarios}>
         <Text style={styles.textdias}> Forma de pago </Text>
-        <SelectList 
-        search={false}
-        setSelected={setSelected2} 
-        data={formadePago} 
-        onSelect={() => setGasto({...gasto, "FormadePago": selected2})}
-        boxStyles={{marginStart: 5, marginTop: 5}}
+        <SelectDropdown
+        data={formadePago}
+        buttonStyle={styles.dropdown}
+        onSelect={(selectedItem, index) => {
+          setGasto({...gasto, "FormadePago": selectedItem, "FormadePagoIndex": index})
+        }}
+        // defaultValueByIndex= "2"
+        // {gasto.FormadePagoIndex}
         />
       </SafeAreaView>
       <SafeAreaView style={styles.diasyhorarios}>
@@ -108,18 +141,17 @@ const Nuevogasto = (props) => {
       </SafeAreaView>
       <View style={styles.buttton}>
       <Button 
-      containerStyle={{width: 320, marginHorizontal: 0, marginVertical: 30, alignContent: "center"}}
+      containerStyle={styles.buttton}
       title="Agregar"
       onPress={()=> saveNewGasto()} 
-      // onPress={()=> {saveNewGasto}} 
       /> 
       </View>
       <View style={styles.buttton}>
       <Button 
-      containerStyle={{width: 320, marginHorizontal: 0, marginVertical: 30, alignContent: "center"}}
+      containerStyle={styles.buttton}
+      // containerStyle={{width: 320, marginHorizontal: 0, marginVertical: 30, alignContent: "center"}}
       title="Ver gastos"
       onPress={()=> props.navigation.navigate("Vergastos")} 
-      // onPress={()=> {saveNewGasto}} 
       /> 
       </View>
     </View>
@@ -136,7 +168,7 @@ const styles = StyleSheet.create({
       marginBottom: 50,
     },
     container: {
-      flex: 1,
+      // flex: 1,
       // backgroundColor: "#fff",
       marginTop: 80,
     },
@@ -144,16 +176,24 @@ const styles = StyleSheet.create({
       flexDirection: "row",
       // backgroundColor: "#fff",
     //   alignItems: "center",
-      justifyContent: "flex-start",
+      // justifyContent: "flex-start",
     //   flex: 1,
     },
     textdias: {
       fontSize: 20,
+      // backgroundColor: 'blue',
+      width: 200,
+      alignContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+      textAlignVertical: 'center'
     },
     textdiaserror: {
       fontSize: 17,
       color: "red",
       textAlign: "right",
+      width: 200
+
     },
     input: {
         height: 50,
@@ -169,41 +209,69 @@ const styles = StyleSheet.create({
       },
     input2: {
       height: 50,
-      margin: 5,
+      // margin: 5,
       borderWidth: 0.5,
       padding: 10,
-      minWidth: 155,
+      minWidth: 200,
       // maxWidth: 1000,
       fontSize: 15,
     //   justifyContent: "flex-end",
-      marginStart: 100,
+      // marginStart: 100,
       borderRadius: 10,
     },
     input3: {
       height: 60,
-      margin: 5,
+      // margin: 5,
       borderWidth: 0.5,
       padding: 10,
-      minWidth: 155,
+      minWidth: 200,
       maxWidth: 225,
       fontSize: 15,
     //   justifyContent: "flex-end",
-      marginStart: 44.5,
+      // marginStart: 44.5,
       borderRadius: 10,
+      marginTop: 10
     },
     hidden: {
      hidden: false,
      height: 0
     },
     buttton: {
-      flexDirection: 'row',
-      justifyContent: "center",
-  
+      width: 320, 
+      // marginHorizontal: 0, 
+      // marginVertical: 30, 
+      alignContent: "center",
+      marginTop: 10,
+      marginStart: 25
+      
+      // alignItems: 'center'
+      // flexDirection: 'row',
+      //  justifyContent: "center",
+    // justifyContent: 'center',
+    //  alignItems: 'center',
+    // borderWidth: 1,
+    // borderColor: '#444',
     },
     buttton2: {
       alignItems:"center",
-      marginTop: 10
+      marginTop: 0
     },
+    dropdown: {
+    // flexShrink: 1000,
+    // justifyContent: 'center',
+    // justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 0.5,
+    borderColor: '#444',
+    borderRadius: 10,
+    width: 200,
+    // position: 'relative',
+    // marginEnd: 10,
+    alignContent: 'center',
+    marginTop: 10
+    
+
+    }
   });
 
 export default Nuevogasto
--- screens/Ver gastos.js
+++ screens/Ver gastos.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button, StyleSheet } from "react-native";
 import { ListItem } from "@rneui/themed";
-// import { ListItem, Avatar } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
 import { db } from '../database/firebase.js';
 import { collection, onSnapshot, orderBy, query} from 'firebase/firestore';
@@ -25,30 +24,6 @@ const Vergastos = (props) => {
     ) })
           return unsuscribe
   }, []);
-  const categorias = [
-    {key:'1',value:'Salud'},
-    {key:'2',value:'Supermercado'},
-    {key:'3',value:'Niñera/Limpieza'},
-    {key:'4',value:'Colegio'},
-    {key:'5',value:'Verdulería'},
-    {key:'6',value:'Impuestos'},
-    {key:'7',value:'Pago Tarjeta de Crédito'},
-    {key:'8',value:'Ropa'},
-    {key:'9',value:'Carnicería'},
-    {key:'10',value:'Librería'},
-    {key:'11',value:'Otros gastos'}
-];
-const formadePago = [
-    {key:'1',value:'Tarjeta de Crédito'},
-    {key:'2',value:'Tarjeta de Débito'},
-    {key:'3',value:'Efectivo'},
-    {key:'4',value:'Billetera Santa Fe con descuesto'},
-    {key:'5',value:'Billetera Santa Fe sin descuesto'},
-    {key:'6',value:'Personal Pay con descuento '},
-    {key:'7',value:'Donaciones'},
-    {key:'8',value:'Otros'},
-];
-// console.log(categorias.find(categoria => categoria.key === gastos.Categoria));
 
   return (
     <ScrollView>
@@ -73,7 +48,7 @@ const formadePago = [
               <ListItem.Subtitle>Categoría: {gasto.Categoria}</ListItem.Subtitle>
               <ListItem.Subtitle>Forma de pago: {gasto.FormadePago}</ListItem.Subtitle>
               <ListItem.Subtitle>Comentario: {gasto.Comentario}</ListItem.Subtitle>
-              <ListItem.Subtitle>Fecha de carga: {Date(gasto.createdAt)}</ListItem.Subtitle>
+              <ListItem.Subtitle>Fecha de carga: {new Date(gasto.createdAt.toDate()).toString().slice(0,-15)}</ListItem.Subtitle>
             </ListItem.Content>
           </ListItem>
         );
